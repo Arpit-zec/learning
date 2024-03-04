e8e2bd30cb4c031183412e41dcda1e7d871ec9ca
Arpit-zec <arpit.s@zecdata.com>
2024-03-04 16:50:24 +0530

    second file added

---
+++ lambda/second_lambda.py
@@ -0,0 +1,19 @@
+import json
+
+def lambda_handler(event, context):
+    
+    cre = creator()
+    pur = purpose()
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!'),
+        'creator': json.dumps(cre),
+        'purpose': json.dumps(pur)
+    }
+
+def creator():
+    return "arpit sahu"
+    
+def purpose():
+    return "learning"
+
